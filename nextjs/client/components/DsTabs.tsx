b03b239ebc6f749aaab56fb007f67a43e3b986c0
LaurellCuza <laurels129@Knights.ucf.edu>
2021-11-03 22:45:11 -0400

    Database Advanced Search

--- nextjs/client/components/DsTabs.tsx
+++ nextjs/client/components/DsTabs.tsx
@@ -5,6 +5,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { Button, TextField } from '@mui/material';
 
+
 interface TabPanelProps {
   children?: React.ReactNode;
   index: number;
@@ -55,18 +56,83 @@ export default function BasicTabs() {
           <Tab label="Tobacco Entry" {...a11yProps(3)} />
         </Tabs>
       </Box>
-      <TabPanel value={value} index={0}>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <Button variant="contained">Contained</Button>
+
+      <TabPanel value={value} index={0}> 
+      <TextField id="outlined-basic" label="Enter" variant="outlined" />
+      <Button variant="contained">Search</Button>
       </TabPanel>
+
       <TabPanel value={value} index={1}>
-        Item Two
+      <TextField id="outlined-basic" label="Reel" variant="outlined" />
+      <TextField id="outlined-basic" label="Store Owner" variant="outlined" />
+      <TextField id="outlined-basic" label="Folio Year" variant="outlined" />
+      <TextField id="outlined-basic" label="Folio Page" variant="outlined" />
+      <TextField id="outlined-basic" label="Entry ID" variant="outlined" />
+      <TextField id="outlined-basic" label="Account Holder Name" variant="outlined" />
+      
+      <TextField id="outlined-basic" label="People" variant="outlined" />
+      <TextField id="outlined-basic" label="Places" variant="outlined" />
+      <TextField id="outlined-basic" label="Commodity" variant="outlined" />
+      <TextField id="outlined-basic" label="Colony" variant="outlined" />
+      <TextField
+          id="outlined-multiline-static"
+          label="Entry Description"
+          multiline
+          rows={4}
+          defaultValue="..."
+        />
+      <TextField id="outlined-basic" label="Tobacco Mark Name" variant="outlined" />
+      <Button variant="contained">Search</Button>
       </TabPanel>
+
       <TabPanel value={value} index={2}>
-        Item Three
+      <TextField id="outlined-basic" label="Reel" variant="outlined" />
+      <TextField id="outlined-basic" label="Store Owner" variant="outlined" />
+      <TextField id="outlined-basic" label="Folio Year" variant="outlined" />
+      <TextField id="outlined-basic" label="Folio Page" variant="outlined" />
+      <TextField id="outlined-basic" label="Entry ID" variant="outlined" />
+      <TextField id="outlined-basic" label="Account Holder Name" variant="outlined" />
+    
+      <TextField id="outlined-basic" label="People" variant="outlined" />
+      <TextField id="outlined-basic" label="Places" variant="outlined" />
+      <TextField id="outlined-basic" label="Commodity" variant="outlined" />
+      <TextField id="outlined-basic" label="Colony" variant="outlined" />
+      <TextField
+          required
+          id="outlined-required"
+          label="Per Order (Yes or No)"
+          defaultValue="1 for Yes, 0 for no"
+        />
+      <TextField id="outlined-basic" label="Item Name" variant="outlined" />
+      <TextField id="outlined-basic" label="Category" variant="outlined" />
+      <TextField id="outlined-basic" label="Sub-Category" variant="outlined" />
+      <TextField id="outlined-basic" label="Varient" variant="outlined" />
+      <Button variant="contained">Search</Button>
       </TabPanel>
+
       <TabPanel value={value} index={3}>
-        Item Three
+      <TextField id="outlined-basic" label="Reel" variant="outlined" />
+      <TextField id="outlined-basic" label="Store Owner" variant="outlined" />
+      <TextField id="outlined-basic" label="Folio Year" variant="outlined" />
+      <TextField id="outlined-basic" label="Folio Page" variant="outlined" />
+      <TextField id="outlined-basic" label="Entry ID" variant="outlined" />
+      <TextField id="outlined-basic" label="Account Holder Name" variant="outlined" />
+    
+      <TextField id="outlined-basic" label="People" variant="outlined" />
+      <TextField id="outlined-basic" label="Places" variant="outlined" />
+      <TextField id="outlined-basic" label="Commodity" variant="outlined" />
+      <TextField id="outlined-basic" label="Colony" variant="outlined" />
+      <TextField
+          id="outlined-multiline-static"
+          label="Entry Description"
+          multiline
+          rows={4}
+          defaultValue="..."
+        />
+      <TextField id="outlined-basic" label="Tobacco Mark Name" variant="outlined" />
+      <TextField id="outlined-basic" label="Note Number" variant="outlined" />
+      <TextField id="outlined-basic" label="Money Type" variant="outlined" />
+      <Button variant="contained">Search</Button>
       </TabPanel>
     </Box>
   );
--- nextjs/pages/databasesearch.tsx
+++ nextjs/pages/databasesearch.tsx
@@ -1,12 +1,12 @@
 import DsTabs from '@components/DsTabs';
 import Header from '@components/Header';
+
 import React from 'react';
 
 const databasesearch = () => {
     return (
         <div>
             <Header />
-            
             <DsTabs/>
         </div>
     );
