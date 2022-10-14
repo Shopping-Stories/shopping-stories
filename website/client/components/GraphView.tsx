4d1ac30c728bffa373de27a78270ab7f27b1de34
J-VOL <57736432+J-VOL@users.noreply.github.com>
2022-10-14 00:11:46 -0400

    Graph view skeleton + some fixes to entry search

---
+++ website/client/components/GraphView.tsx
@@ -0,0 +1,17 @@
+import Box from '@mui/material/Box'
+import { ForceGraph2D } from "react-force-graph";
+import { ForceGraph3D } from "react-force-graph";
+
+
+interface GraphVisual {
+
+};
+
+const GraphVisual = (props: GraphVisual): JSX.Element => {
+    return(
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        </Box>
+    )
+};
+
+export default GraphVisual;
