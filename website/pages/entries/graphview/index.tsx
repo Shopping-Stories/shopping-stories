518240b29cbaa07dee08baf28ed5e4ef1131b70a
J-VOL <57736432+J-VOL@users.noreply.github.com>
2022-10-16 22:37:24 -0400

    generated types/query-hooks for all queries.

---
+++ website/pages/entries/graphview/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import Header from '@components/Header';
+
+const GraphViewPage: NextPage = () => {
+    return (
+        <>
+            <Header/>
+        </>
+    );
+};
+
+export default GraphViewPage;
