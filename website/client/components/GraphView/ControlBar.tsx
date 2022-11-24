72c69c7ad7de99b00f2167c71a47d2c105c7e3c6
J-VOL <57736432+J-VOL@users.noreply.github.com>
2022-11-23 20:30:26 -0500

    -info panel updates, dummy filters and search

--- website/client/components/GraphView/ControlBar.tsx
+++ website/client/components/GraphView/ControlBar.tsx
@@ -1,15 +1,22 @@
-import { useState, memo, useEffect } from "react";
+import React, { useState, memo, useEffect } from "react";
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import PersonIcon from '@mui/icons-material/Person';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import StorefrontIcon from '@mui/icons-material/Storefront';
+import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import ShoppingBasketOutlinedIcon from '@mui/icons-material/ShoppingBasketOutlined';
 import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
+import HistoryEduOutlinedIcon from '@mui/icons-material/HistoryEduOutlined';
 import PlaceIcon from '@mui/icons-material/Place';
+import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined';
 import NoteIcon from '@mui/icons-material/Note';
+import NoteOutlinedIcon from '@mui/icons-material/NoteOutlined';
 import StickyNote2Icon from '@mui/icons-material/StickyNote2';
+import StickyNote2OutlinedIcon from '@mui/icons-material/StickyNote2Outlined';
 import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
 import LegendToggleIcon from '@mui/icons-material/LegendToggle';
@@ -22,6 +29,20 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import Box from '@mui/material/Box';
 import { NodeInfo } from '@components/GraphView/GraphTypes';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import CloseIcon from '@mui/icons-material/Close';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import FormGroup from '@mui/material/FormGroup';
+import Checkbox from '@mui/material/Checkbox';
+import SearchIcon from '@mui/icons-material/Search';
+import TextField from '@mui/material/TextField';
+import Slider from '@mui/material/Slider';
+import OutlinedInput from '@mui/material/OutlinedInput';
 // import DescriptionIcon from '@mui/icons-material/Description';
 // import InfoIcon from '@mui/icons-material/Info';
 // import MenuOpenIcon from '@mui/icons-material/MenuOpen';
@@ -60,6 +81,36 @@ const DrawerHeader = styled('div')(({ theme }) => ({
     ...theme.mixins.toolbar,
     justifyContent: 'flex-start',
 }));
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+    
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box>
+                    {children}
+                </Box>
+            )}
+        </div>
+    );
+}
+function a11yProps(index: number) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 
 interface ControlBarProps {
     // width: number;
@@ -69,10 +120,15 @@ interface ControlBarProps {
 const ControlBar = ({ info, name }: ControlBarProps) => {
     const [open, setOpen] = useState(false);
     const [infoOpen, setInfoOpen] = useState(!!info);
+    const [tab, setTab] = useState(0)
     
     const handleClick = () => {
         setOpen(!open);
     };
+    
+    const handleTabSwitch = (_event: React.SyntheticEvent,value:number) => {
+        setTab(value)
+    }
 
     const openInfo = () => {
         setInfoOpen(!infoOpen);
@@ -185,16 +241,24 @@ const ControlBar = ({ info, name }: ControlBarProps) => {
                 open={infoOpen}
             >
                 <Toolbar />
+                <Box>
                 <DrawerHeader>
                     <IconButton onClick={openInfo}>
                         {theme.direction === 'rtl' ? (
                             <ChevronLeftIcon />
                         ) : (
                             <ChevronRightIcon />
+                            // <CloseIcon />
                         )}
                     </IconButton>
+                    <Tabs value={tab} onChange={handleTabSwitch}>
+                        <Tab label="Selection Info" {...a11yProps(0)}/>
+                        <Tab label="Search and Filter"{...a11yProps(1)}/>
+                    </Tabs>
                 </DrawerHeader>
                 <Divider />
+                
+                <TabPanel index={0} value={tab}>
                 <List>
                     <ListItem>
                         <ListItemText primary={name}/>
@@ -216,7 +280,66 @@ const ControlBar = ({ info, name }: ControlBarProps) => {
                                 </ListItem>
                         ))}
                 </List>
+                </TabPanel >
+                <TabPanel index={1} value={tab}>
+                    <Box sx={{ display: 'flex', alignItems: 'flex-end', flexWrap: 'wrap' }}>
+                    <FormControl component="fieldset" variant="outlined">
+                            {/*<div>*/}
+                            {/*<SearchIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />*/}
+                            <TextField
+                                id="input-with-sx"
+                                label="Search..."
+                                sx={{ m: 1, width: '25ch' }}
+                                variant="standard"
+                            />
+                            {/*</div>*/}
+                        <Divider/>
+                        {/*<Box>*/}
+                        <div>
+                        <FormLabel sx={{ml:1}}>Node Category Filters</FormLabel>
+                            <FormGroup row>
+                            <Checkbox
+                                checkedIcon={<PersonIcon/>}
+                                icon={<PersonOutlinedIcon/>}
+                            />
+                            <Checkbox
+                                checkedIcon={<PlaceIcon/>}
+                                icon={<PlaceOutlinedIcon/>}
+                            />
+                            <Checkbox
+                                checkedIcon={<StorefrontIcon/>}
+                                icon={<StorefrontOutlinedIcon/>}
+                            />
+                            <Checkbox
+                                checkedIcon={<NoteIcon/>}
+                                icon={<NoteOutlinedIcon/>}
+                            />
+                            <Checkbox
+                                checkedIcon={<ShoppingBasketIcon/>}
+                                icon={<ShoppingBasketOutlinedIcon/>}
+                            />
+                            <Checkbox
+                                checkedIcon={<StickyNote2Icon/>}
+                                icon={<StickyNote2OutlinedIcon/>}
+                            />
+                            <Checkbox
+                                checkedIcon={<HistoryEduIcon/>}
+                                icon={<HistoryEduOutlinedIcon/>}
+                            />
+                        </FormGroup>
+                        </div>
+                    </FormControl>
+                </Box>
+                <div>
+                    <Typography sx={{ml:1}}>Date Range</Typography>
+                    <Slider
+                        sx={{ml:2, mr:2}}
+                    />
+                </div>
+                    
+                </TabPanel>
                 <Divider />
+                </Box>
             </Drawer>
         </Box>
     );
